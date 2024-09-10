2455729a8a3d95870067e6c47d55b515fcb12ace
timothygilmore <timothy.r.gilmore4.civ@us.navy.mil>
2024-09-10 19:42:35 +0000

    add figlet and print statements and breakout function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 //use std::vec;
 use rand::Rng;
+use figleter::FIGfont;
+
 fn main() {
+    let standard_font = FIGfont::standard().unwrap();
+    let banner = standard_font.convert("Passgen");
+
+    println!("{}", banner.unwrap());
+    println!("Here is your password: \n\n");
+
+    fn generate_standard() -> String {
     let char_bank: [char; 65] = [
     '0', '1', '2', '3', '4',
     '5', '6', '7', '8', '9',
@@ -19,11 +28,14 @@ fn main() {
     const FILLER: char = 'a';
 
     let mut pass_array: [char; 16]= [FILLER; 16];
-    for x in 1..16 {
+    for x in 0..16 {
         let  c = rand::thread_rng().gen_range(0..=64);
         pass_array[x] = char_bank[c];
     }
     let password = String::from_iter(pass_array);
-    print!("{}",password);
-    
+    password
+    }
+    println!("{}\n\n",generate_standard());
+    println!("Do you want to keep a record of this password? (Yes)\n1. Yes\n2. New Standard Password\n3. Quit")
+      
 }
